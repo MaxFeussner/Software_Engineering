ec965455e1be23e63d0632d9b3838fc237269729
Andrés <aog25.2014@gmail.com>
2019-11-18 13:13:19 +0000

    Added comments to VM.c

--- Implementation/VM/VM.c
+++ Implementation/VM/VM.c
@@ -12,19 +12,28 @@
 //constant vector of instructions available
 const char* INSTRUCTION_STRING[INSTR] = {"LDI", "LDF", "ADD", "SUB", "MUL", "DIV", "MOD"};
 
+
+/*
+ * Pre: ---
+ * Post: returned true if and only if op was a '+', '-', '*', '/' or '%'
+ */
 bool validOperator(char op){
 	return (op=='+' || op=='-' || op=='*' || op=='/' || op=='%');
 }
 
+
+/*
+ * Pre: t and value are not NULL pointer
+ * Post: if t had id "INTEGER_ID" or "DOUBLE_ID", assigned the value to value and returned 0
+ * 	 otherwise, returned -1
+ */
 int transformToDouble(Token* t, double* value){
 	if(getID(t)==INTEGER_ID){
 		int aux = *(int*) getValue(t);
 		*value = aux;
-		//printf("double = %lf\n", *value);
 	}
 	else if(getID(t)==DOUBLE_ID){
 		*value = *(double*) getValue(t);
-		//printf("double = %lf\n", *value);
 	}
 	else return -1;
 	return 0;
@@ -32,11 +41,11 @@ int transformToDouble(Token* t, double* value){
 
 
 /*
- * Pre: "t" is a valid UNARY operator or an OPERATOR as is defined in token.h.
- * 		"output" is a pointer to a valid, open and writeable file
- * 		let: "top" be the top of stack "s" and "n" be the number of elements in "s"
- * Post: if n==0 or the precedence of "t" greater than "top", "t" has been pushed to the top
- * 		 otherwise, the elements have been popped and printed until the precedence of all the elements in "s" is less or equal to "t"
+ * Pre: ---
+ * Post: if op was a valid operator and t1 and t2 were not NULL, returned a token with id "DOUBLE_ID"
+ * 	 holding the value of making the operation v1 op v2, where v1 and v2 are the numerical values
+ * 	 in tokens t1 and t2.
+ *	 otherwise, it printed an error message and returned NULL
  */
 Token* operateTokens(Token* t1, char op, Token* t2){
 
@@ -44,7 +53,14 @@ Token* operateTokens(Token* t1, char op, Token* t2){
 		fprintf(stderr, "Unknown operand \'%c\' in function operateTokens\n", op);
 		return NULL;	
 	}
-
+	if(t1 == NULL){
+		fprintf(stderr, "Cannot operate a NULL token on pointer t1\n");
+		return NULL;
+	}
+	if(t2 == NULL){
+		fprintf(stderr, "Cannot operate a NULL token on pointer t2\n");
+		return NULL;
+	}
 
 	Token* resultToken = createToken(DOUBLE_ID);
 	double v1, v2, res;
@@ -61,33 +77,34 @@ Token* operateTokens(Token* t1, char op, Token* t2){
 	switch(op){
 		case '+':
 			res = v1 + v2;
-			//printf("%lf+%lf=%lf\n", v1,v2,res);
 			setValue(resultToken, &res, sizeof(double));
 			break;
 		case '-':
 			res = v1 - v2;
-			//printf("%lf-%lf=%lf\n", v1,v2,res);
 			setValue(resultToken, &res, sizeof(double));
 			break;
 		case '*':
 			res = v1 * v2;
-			//printf("%lf*%lf=%lf\n", v1,v2,res);
 			setValue(resultToken, &res, sizeof(double));
 			break;
 		case '/':
 			res = v1 / v2;
-			//printf("%lf/%lf=%lf\n", v1,v2,res);
 			setValue(resultToken, &res, sizeof(double));
 			break;
 		case '%':
 			res = fmod(v1, v2);
-			//printf("%lfmod%lf=%lf\n", v1,v2,res);
 			setValue(resultToken, &res, sizeof(double));
 			break;
 	}
 	return resultToken;
 }
 
+/*
+ * Pre: INSTRUCTION_STRING is a vector of INSTR strings containing the identifiers of all the operations
+ *	the Virtual Machine accepts
+ * Post: returned the index in INSTRUCTION_STRING of the instruction stored in buffer.
+ *	 if it was not found in the vector, returned -1
+ */
 int instruction_index(char* buffer){
 	int result = -1;
 	for(int i = 0; i < INSTR; i++){
@@ -102,12 +119,12 @@ int instruction_index(char* buffer){
 
 
 /*
- * Pre: input file (if defined with the option -i, otherwise standard input) has the following format
- *			SPC1:X1
- * 			SPC2:X2
- * 		where SPCi are the specifier strings of the token types (defined above) and Xi are values
- * 		of such type.
- * Post: output file (if defined with the option -o, otherwise standard output) contains the operations from the input file in postfix notation
+ * Pre: input file (if defined with the option -i, otherwise standard input) has a series of instructions from
+ * 	the vector INSTRUCTION_STRING.
+ * 	All instructions in INSTRUCTION_STRING must be of length INSTR_CHARS characters
+ * Post: output file (if defined with the option -o, otherwise standard output) contains the result of performing
+ * 	 such operations if notation was correct.
+ *	 if an unknown operation was found, printed an error message on stderr
  */
 int main(int argc, char** argv){
 	int i = 1;
@@ -131,78 +148,80 @@ int main(int argc, char** argv){
 	if(in_file == NULL) in_file = stdin; //if input file wasn't defined or there were errors, use standard input
 	if(out_file == NULL) out_file = stdout; //if output file wasn't defined or there were errors, use standard output
 
-	char buffer[INSTR_CHARS + 1];
-	char* aux = NULL;
-	size_t aux2 = 0;
-	size_t line_size = INSTR_CHARS;
-	ssize_t readBytes = 0;
-	Stack* s = newStack();
+	char buffer[INSTR_CHARS + 1];	//buffer to read instructions
+	char* aux = NULL;		//buffer to ignore lines
+	size_t aux2 = 0;		//size_t variable (used to ignore lines)
+	size_t line_size = INSTR_CHARS;	//number of characters for every instruction
+	ssize_t readBytes = 0;		//variable where the number of read bytes will be stored after every read
+	Stack* s = newStack();		//stack where all numbers will be pushed
 	Token* t;
-	readBytes = fread(buffer, sizeof(char), line_size, in_file);
-	while(readBytes > 0){	//if the line was valid
+	readBytes = fread(buffer, sizeof(char), line_size, in_file);	//read first instruction
+	while(readBytes > 0){	//if the instruction was valid
 
-		int instr = instruction_index(buffer);
-		if(instr >= 0 && instr < INSTR){
-			if(instr == 0){
+		int instr = instruction_index(buffer);	//get instruction index
+		if(instr >= 0 && instr < INSTR){	//if it's within bounds
+			if(instr == 0){	//LDI
 				t = createToken(INTEGER_ID);
 				int intNumber;
-				fscanf(in_file, " %d", &intNumber);
+				fscanf(in_file, " %d", &intNumber);	//read integer
 				setValue(t, &intNumber, sizeof(int));
 			}
-			else if(instr == 1){
+			else if(instr == 1){	//LDF
 				t = createToken(DOUBLE_ID);
 				double fpNumber;
-				fscanf(in_file, " %lf", &fpNumber);
+				fscanf(in_file, " %lf", &fpNumber);	//read floating point number
 				setValue(t, &fpNumber, sizeof(double));
 			}
 			else{
 				Node* auxNode = popElement(s);
-				Token* secondOperand = (Token*) getData(auxNode);
+				Token* secondOperand = (Token*) getData(auxNode);	//get token from popped element
 				free(auxNode);
 				auxNode = popElement(s);
-				Token* firstOperand = (Token*) getData(auxNode);
+				Token* firstOperand = (Token*) getData(auxNode);	//get token from popped element
 				free(auxNode);
 				switch(instr){
-					case 2:
+					case 2:	//ADD
 						t = operateTokens(firstOperand, '+', secondOperand);
 						break;
-					case 3:
+					case 3: //SUB
 						t = operateTokens(firstOperand, '-', secondOperand);
 						break;
-					case 4:
+					case 4: //MUL
 						t = operateTokens(firstOperand, '*', secondOperand);
 						break;
-					case 5:
+					case 5: //DIV
 						t = operateTokens(firstOperand, '/', secondOperand);
 						break;
-					case 6:
+					case 6: //MOD
 						t = operateTokens(firstOperand, '%', secondOperand);
 						break;
 				}
 				free(firstOperand);
 				free(secondOperand);
 			}
-		pushElement(s, t);
+			if(t != NULL) pushElement(s, t);	//push result
+			else fprintf(stderr, "Error: t is NULL. Couldn't perform operation \"%s\"", buffer);
 		}
 		else fprintf(stderr, "Error: Unknown operation %s\n", buffer);
 
-		
-		getline(&aux, &aux2, in_file);
+		//ignore the rest of the line
+		getline(&aux, &aux2, in_file); 
 		free(aux);
 		aux = NULL;
 		aux2 = 0;
 
-		readBytes = fread(buffer, sizeof(char), line_size, in_file);//read next line
+		readBytes = fread(buffer, sizeof(char), line_size, in_file);	//read next instruction
 	}
-	Node* auxNode2 = popElement(s);
+	Node* auxNode2 = popElement(s);	//pop last element where the result is stored
 	Token* auxToken = getData(auxNode2);
 	
 	double finalResult = *(double*) getValue(auxToken);
 	free(auxNode2);
 	free(auxToken);
-	if(!isEmpty(s)) fprintf(stderr, "Error: Stack is not empty!\n");
-	else fprintf(out_file, "%lf\n", finalResult);
+	if(!isEmpty(s)) fprintf(stderr, "Error: Stack is not empty!\n");	//something went wrong (probably missing operation)
+	else fprintf(out_file, "%lf\n", finalResult);	//print result in out_file
 
+	//free allocated memory
 	fclose(in_file);
 	fclose(out_file);
 	destroyElements(s);
