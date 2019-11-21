ae18d16f8f9202566c971a16b5aae26bcd1d860f
juanvmartinez <32192049+juanvmartinez@users.noreply.github.com>
2019-11-21 14:14:20 +0000

    CodeGenerator simplify

--- Implementation/CodeGenerator/codegenerator.c
+++ Implementation/CodeGenerator/codegenerator.c
@@ -4,28 +4,11 @@
 #include <string.h>
 #include<stdbool.h>
 #include<ctype.h>
-//cambiar
+//it checks if is valid the operator
 bool validOperator(char op){
 	return (op=='+' || op=='-' || op=='*' || op=='/' || op=='%');
 }
-// de que tipo es la salida anterior 
-// pueden 
-//problema con el espacio
-char *concat(char *s1,char *s2){
-	int length = strlen(s1) + strlen(s2);
-	char *result = (char *) malloc(sizeof(char)*(length + 2));
-	int j = strlen(s1);
-	int i;
-	for(i = 0;i < j;i++){
-		result[i] = s1[i];
-	}
-	result[j] = ' ';
-	for(i=0;i<strlen(s2);i++){
-		result[i + j + 1 ] = s2[i];
-	} 
-	result[length + 1] = '\0';
-	return result;
-}
+//it checks if the input is a int float or char
 int tokentype(char *t){
 	bool canbeint = true;
 	bool canbefloat = false;
@@ -52,27 +35,27 @@ int tokentype(char *t){
 			canbeint = false;
 		}
 	}
-	if(canbefloat) return 1;
-	else if(canbeint) return 0;
+	if(canbefloat) {printf("es float\n"); return 1; }
+	else if(canbeint) {printf("es int\n"); return 0;}
 	else return -1;
 	
 }
+//it returns the instruccion for each input
 char *codeGenerator(char *i){
 	char* buff = (char*) calloc(20, sizeof(char));
 	int type = tokentype(i);
 	if(type == 0){
-			//Error solucionar gcvt pasar a char);
-		//code = concat("LDI",gcvt(i,20,buff));
+			
 		strcpy(buff, "LDI ");
     strcat(buff, i);
 	}
 	if(type == 1){
-	//	code = concat("LDF",gcvt(i,20,buff));
+	
 		strcpy(buff, "LDF ");
     strcat(buff, i);
 	}
 	else if(type == 3){
-		//Solucionar me pide que sea un int
+	
 		switch(i[0]){
 		
 			case '+':
@@ -97,19 +80,16 @@ char *codeGenerator(char *i){
 		return buff;
 
 }
-//poner en el main
 
+//calling the other function it writes in the output the instruccions
 int main()
 {
 
-//Hay que usar este
 	FILE *archivo;
 	FILE *output;
  	char caracteres[100];
- 	//archivo = fopen("prueba.txt","r");
-	//output = fopen("output.txt","w");
-	archivo = stdin;
-	output = stdout;
+ 	archivo = fopen("prueba.txt","r");
+	output = fopen("output.txt","w");
  	if (archivo == NULL)
  		exit(1);
  	else
@@ -123,11 +103,11 @@ int main()
 	
 	      if(token != NULL){
 	      while(token != NULL){
-	      // Sï¿½lo en la primera pasamos la cadena; en las siguientes pasamos NULL
+	     
 		    code  = codeGenerator(token);
-	      //	writeFile(file,code);
         fprintf(output,"%s\n", code);
         free(code);
+		    printf("Token: %s\n", token);
 		    token = strtok(NULL, delimitador);
         }
     }
@@ -141,32 +121,4 @@ int main()
 
 
     return 0;
-}
-char convert(int i){
-	return i+'0';
-}
-
-
-//leer caracter 
-char *readFile(char *fileName)
-{
-    FILE *file = fopen(fileName, "r");
-    char *code;
-    size_t n = 0;
-    int c;
-
-    if (file == NULL)
-        return NULL; //could not open file
-
-    code = malloc(1000);
-
-    while ((c = fgetc(file)) != EOF)
-    {
-        code[n++] = (char) c;
-    }
-
-    // don't forget to terminate with the null character
-    code[n] = '\0';        
-
-    return code;
 }
