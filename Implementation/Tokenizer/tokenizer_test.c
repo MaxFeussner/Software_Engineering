2895d14a47d24599cdb894a6ab7aa114a7414d63
Maximilian Feussner <mf18@csg21-22.ucc.ie>
2019-11-25 16:16:16 +0000

    some changes in the testsuite for the tokenizer

--- Implementation/Tokenizer/tokenizer_test.c
+++ Implementation/Tokenizer/tokenizer_test.c
@@ -8,23 +8,25 @@
 #include <ctap.h>
 
 TESTS{
-
-int tokenizer(){
-  FILE * in_file = fopen("test_input_tokenizer","r");
-  FILE * out_file = fopen("test_output_tokenizer", "a");
-  char* buffer;
-  buffer = (char*) malloc(100 * sizeof(char));
-  //get_buffer(in_file, buffer);
-  //iterate_over_buffer(buffer, out_file);
-  //fclose(in_file);
-  return 0;
+int tokenizer(FILE * in_file, FILE * out_file){
+char* buffer;
+buffer = (char*) malloc(1000*sizeof(char));
+get_buffer(in_file, buffer);
+iterate_over_buffer(buffer, out_file);
 }
 
 char* buffer1;
-buffer1 = (char*) malloc(100*sizeof(char));
+buffer1 = (char*) malloc(1000*sizeof(char));
+FILE * input_file = fopen("test_input_tokenizer.txt", "r");
+FILE * out_file = fopen("test_output1", "w");
+
+char test_buffer1[]="Un:-\nUn:-\nIn:2\nOp:+\nUn:-\nLp:(\nIn:4\nOp:+\nUn:-\nUn:+\nIn:3\nRp:)\nOp:-\nUn:-\nUn:-\nFp:2.5\nOp:/\nIn:3\n";
+
+ok(tokenizer(input_file, out_file)==0,"The tokenizer was succesfully run");
+fclose(out_file);
 FILE * out_file1 = fopen("test_output1", "r");
-char test_buffer1[]="Un:-\nUn:-\nIn:2\nOp:+\nUn:-\nLp:(\nIn:4\nOp:+\nUn:-\nUn:+\nIn:3\nRp:)\nOp:-\nUn:-\nUn:-\nFp:2.5\nOp:/\nIn:3";
+
 is(get_buffer(out_file1,buffer1),test_buffer1,"The input was correctly split into tokens.");
-ok(tokenizer()==0,"HEZ");
+
 
 }
